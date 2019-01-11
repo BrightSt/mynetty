34f6d99fa56ff786c49258d1c11f896794e8725b
BrightSt <dongmingsheng20@163.com>
2019-01-11 17:17:18 +0800

    init

---
+++ src/main/java/com/dms/mynetty/MynettyApplication.java
@@ -0,0 +1,14 @@
+package com.dms.mynetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MynettyApplication.class, args);
+    }
+
+}
+
